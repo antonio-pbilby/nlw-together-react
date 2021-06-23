ede90da6d3ec7a87efb75117f48f7c3a137d7775
Antônio Bilby <antoniobilbylemos@gmail.com>
2021-06-23 18:13:20 -0300

    ugh

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,21 +6,21 @@ export function Home() {
   return (
     <div>
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e repostas"/>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e repostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real.</p>
       </aside>
       <main>
         <div>
-          <img src={logoImg} alt="Letmeask"/>
+          <img src={logoImg} alt="Letmeask" />
           <button>
-            <img src={googleIconImg} alt="Logo do Google"/>
+            <img src={googleIconImg} alt="Logo do Google" />
           </button>
           <div>
             ou entre em uma sala
           </div>
           <form>
-            <input type="text" placeholder="Digite o código da sala"/>
+            <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">
               Entrar na sala
             </button>
